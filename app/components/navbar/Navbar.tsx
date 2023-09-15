05ac7cf3243bd99f323ad801e2f238d54e330331
Murod Kusherbaev <muradkusherbaev@gmail.com>
2023-09-16 00:47:17 +0500

    add: navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
 
-const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
+import Container from "../Container";
+
+import Search from "./Search";
+
+import Logo from "./Logo";
+import UserMenu from "./UserMenu";
+
+interface NavbarProps {
+    currentUser?: any;
 }
 
-export default Navbar
+const Navbar: React.FC<NavbarProps> = ({
+    currentUser,
+}) => {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div
+                className="
+          py-4 
+          border-b-[1px]
+        "
+            >
+                <Container>
+                    <div
+                        className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+                    >
+                        <Logo />
+                        <Search />
+                        <UserMenu currentUser={currentUser} />
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
+}
+
+
+export default Navbar;
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,117 @@
+'use client';
+
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
+
+
+
+
+
+const UserMenu: React.FC<any> = ({
+  currentUser
+}) => {
+  const router = useRouter();
+
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="
+            hidden
+            md:block
+            text-sm 
+            font-semibold 
+            py-3 
+            px-4 
+            rounded-full 
+            hover:bg-neutral-100 
+            transition 
+            cursor-pointer
+          "
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={toggleOpen}
+          className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <MenuItem
+              label="My trips"
+              onClick={() => router.push('/trips')}
+            />
+            <MenuItem
+              label="My favorites"
+              onClick={() => router.push('/favorites')}
+            />
+            <MenuItem
+              label="My reservations"
+              onClick={() => router.push('/reservations')}
+            />
+            <MenuItem
+              label="My properties"
+              onClick={() => router.push('/properties')}
+            />
+            <MenuItem
+              label="Airbnb your home"
+              onClick={() => console.log()}
+            />
+            <hr />
+            <MenuItem
+              label="Logout"
+              onClick={() => console.log()}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserMenu;
